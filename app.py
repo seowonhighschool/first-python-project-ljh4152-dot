16c5ae1bb14f372707110c197570e4221ce96cd0
ljh4152 <ljh4152@goedu.kr>
2026-05-15 16:23:52 +0900

    adada

--- app.py
+++ app.py
@@ -1,2 +1,7 @@
-name = input("그대는 누구인가s?이: ")
-print(f"{name}님, ATLAS에 오신 것을 환영합니다!")
+import streamlit as st
+ 
+st.title("실습")
+ 
+name = st.text_input("이름을 입력하세요")
+if name:
+    st.write(f"{name}님 안녕하세요")
